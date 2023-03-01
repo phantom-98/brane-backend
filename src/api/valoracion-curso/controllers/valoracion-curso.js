92f8e454ab819e37cc4fae0b15143d96263b6288
Anchorquery <anchorquery@gmail.com>
2023-03-01 18:36:07 -0400

    Update valoracion-curso.js

--- src/api/valoracion-curso/controllers/valoracion-curso.js
+++ src/api/valoracion-curso/controllers/valoracion-curso.js
@@ -16,43 +16,27 @@ module.exports = createCoreController(
 
       const user = ctx.state.user;
 
-      //obtengo el id del curso al que se le quiere crear la valoración
-
-      console.log(ctx.request.body);
+      if (!user) {
+        return ctx.unauthorized(`You can't create this entry`);
+      }
 
       
               
       const id = ctx.request.body.data.curso;
 
-      //obtengo el curso al que se le quiere crear la valoración 
-
-      const curso = await strapi.db.query("api::curso.curso").findOne({ id: id });
-
-
-     //verifico si el curso que se quiere valorar existe
-
-        if (!curso) {
-            return ctx.notFound("Curso no encontrado");
-        }
-
-      // verifico en la tabla mis cursos si el usuario que está haciendo la petición tiene el curso que se quiere valorar
-
-
-        const misCursos = await strapi.db.query("api::mis-curso.mis-curso").findOne({
+      const misCursos = await strapi.db.query("api::mis-curso.mis-curso").findOne({
             where: { usuario: user.id, curso: id },
-          });
+      });
         
         //console.log(misCursos)
 
       //    si el usuario que está haciendo la petición no está logueado, no puede crear la valoración
 
-      if (!user) {
-        return ctx.unauthorized(`You can't create this entry`);
-      }
+
 
       // si el usuario que está haciendo la petición no está inscrito en el curso y no es administrador, no puede crear la valoración
 
-      if (misCursos.length == 0 && user.role.type != "administrador") {
+      if (!misCursos && user.role.type != "administrador") {
         return ctx.unauthorized(`You can't create this entry`);
       }
 
