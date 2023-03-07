12ca770452cb0084c2be4d3fbc2a5d9035a1870e
Glendys96 <pitterglendys@gmail.com>
2023-03-07 19:33:57 -0400

    mis cursos

--- src/api/mis-curso/controllers/mis-curso.js
+++ src/api/mis-curso/controllers/mis-curso.js
@@ -8,8 +8,16 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::mis-curso.mis-curso', ({ strapi }) => ({
 
+//modifico el metodo find para que me traiga los cursos que estan en el usuario logueado
+    async findMany(ctx) {
 
 
+        const { user } = ctx.state;
+
+        const entities = await strapi.services['mis-curso'].findMany({ usuario: user.id });
+
+        return entities.map(entity => strapi.services['mis-curso'].sanitizeEntity(entity, { model: strapi.models['mis-curso'] }));
+    }
 
 })
 )
