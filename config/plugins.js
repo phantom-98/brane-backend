a8ea554372b0ee9bc4a1fd055ff32db149e6f89d
Glendys96 <pitterglendys@gmail.com>
2023-03-14 18:14:37 -0400

    slug

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,31 @@
 module.exports = ({ env }) => ({
-
-	slugify: {
-		enabled: true,
-		config: {
-			slugifyWithCount: true,
-			shouldUpdateSlug: true,
-			contentTypes: {
-				curso: {
-					field: 'slug',
-					references: 'name',
-				},
-			},
+  slugify: {
+    enabled: true,
+    config: {
+      slugifyWithCount: true,
+      shouldUpdateSlug: true,
+      contentTypes: {
+        curso: {
+          field: "slug",
+          references: "name",
+        },
+        user: {
+          field: "slug",
+          references: ["nombre", "apellidos"],
+        },
+		clases: {	
+		  field: "slug",
+		  references: "nombre",
 		},
-	},
-	// añado condicional para que solo se ejecute en producción
-
-	/*upload: env('NODE_ENV') === 'production' ? {
-		config: {
-			provider: 'cloudinary',
-			providerOptions: {
-				cloud_name: env('CLOUDINARY_NAME'),
-				api_key: env('CLOUDINARY_KEY'),
-				api_secret: env('CLOUDINARY_SECRET'),
-			},
-			actionOptions: {
-				upload: {},
-				delete: {},
-			},
+		categoria: {
+			field: "slug",
+			references: "nombre",
 		},
-	} : {},*/
-
+		cupon: {
+			field: "slug",
+			references: "nombre",
+		},
+      },
+    },
+  },
 });
