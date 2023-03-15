8e6199cf1d6873c6fa5d214c51fa1e4cd9dcde4a
Anchorquery <anchorquery@gmail.com>
2023-03-15 19:00:04 -0400

    add smtp

--- config/plugins.js
+++ config/plugins.js
@@ -28,4 +28,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      }
+    },
+  },
 });
