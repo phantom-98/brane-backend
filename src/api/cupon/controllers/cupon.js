57b524dbd36f198cbda322c7f1a36be36e0349ee
Glendys96 <pitterglendys@gmail.com>
2023-04-06 10:07:56 -0400

    cupon

--- src/api/cupon/controllers/cupon.js
+++ src/api/cupon/controllers/cupon.js
@@ -16,6 +16,8 @@ module.exports = createCoreController('api::cupon.cupon', ({ strapi }) => ({
 
         const user = ctx.state.user;
 
+        console.log(user.id)
+
         //	si el usuario que está haciendo la petición no está logueado, no puede crear el cupon
 
         if (!user) {
@@ -28,13 +30,169 @@ module.exports = createCoreController('api::cupon.cupon', ({ strapi }) => ({
 
         if (user.role.type != "instructor" && user.role.type != "administrador") {
 
-            return ctx.unauthorized(`You can't create this entry`);
+            return ctx.unauthorized(`No tienes permisos para crear este cupon`);
+
+        }
+        //se debe pasar un nombre, valor y tipo para el cupon
+
+        const nombre = ctx.request.body.data.nombre;
+
+        const valor = ctx.request.body.data.valor;
+
+        const tipo = ctx.request.body.data.tipo;
+
+        //si no se envia el nombre del cupon, envio un error
+
+        if (!nombre) {
+
+            return ctx.badRequest("Error", {messages: "No se envio el nombre del cupon"});
 
         }
 
-        // si el usuario que está haciendo la petición es instructor o es administrador, puede crear el cupon
+        //si no se envia el valor del cupon, envio un error
 
-        return await super.create(ctx);
 
+        if (!valor) {
+
+
+            return ctx.badRequest("Error", {messages: "No se envio el valor del cupon"});
+
+        }
+
+        //si no se envia el tipo del cupon, envio un error
+
+        if (!tipo) {
+
+            return ctx.badRequest("Error", {messages: "No se envio el tipo del cupon"});
+
+        }
+
+        //asigno el usuario que hace la peticion al campo user del cupon que se quiere crear
+
+        
+
+        console.log("Este es el usuario", ctx.request.body.data);
+
+        // si el usuario que está haciendo la petición es instructor o es administrador se crea el cupon
+
+            const entity = await strapi.db.query('api::cupon.cupon').create({
+
+                data:{...ctx.request.body.data, user: user.id}
+
+            });
+
+            return entity;
+    },
+
+    //modifico el update para que los cupones sean actualizados solo por usuario que lo creo y administrador
+
+    async update(ctx) {
+
+        // obtengo el usuario que está haciendo la petición
+
+        const user = ctx.state.user;
+
+        //obtengo el id del cupon que se quiere actualizar
+
+        const id = ctx.params.id;
+
+
+        //	si el usuario que está haciendo la petición no está logueado, no puede actualizar el cupon
+
+        if (!user) {
+
+            return ctx.unauthorized(`You can't update this entry`);
+
+        }
+
+        // si el usuario que está haciendo la petición no es instructor ni es administrador, no puede actualizar el cupon
+
+        if (user.role.type != "instructor" && user.role.type != "administrador") {
+
+            return ctx.unauthorized(`No tienes permisos para actualizar este cupon`);
+
+        }
+
+        //verifico que el cupon que se quiere actualizar pertenezca al usuario que lo quiere actualizar
+
+        const cupon = await strapi.db
+
+            .query("api::cupon.cupon")
+
+            .findOne({ where: { id }, populate: true });
+
+            console.log("Este es el cupon", cupon);
+
+        //si el usuario que hace la peticion no es el mismo que creo el cupon y no es administrador, envio un error
+
+        if (user.id != cupon.user.id && user.role.type != "administrador") {
+
+            return ctx.badRequest("Error", 
+
+                { messages: "No tienes permisos para actualizar el cupon" },
+
+            );
+
+        }
+
+        // si el usuario que está haciendo la petición es instructor o es administrador, puede actualizar el cupon
+
+        return await super.update(ctx);
+
+    },
+
+    //modifico el delete para que los cupones sean eliminados solo por usuario que lo creo y administrador
+
+    async delete(ctx) {
+
+        // obtengo el usuario que está haciendo la petición
+
+        const user = ctx.state.user;
+
+        //obtengo el id del cupon que se quiere eliminar
+
+        const id = ctx.params.id;
+
+        //	si el usuario que está haciendo la petición no está logueado, no puede eliminar el cupon
+
+        if (!user) {
+
+            return ctx.unauthorized(`You can't delete this entry`);
+
+        }
+
+        // si el usuario que está haciendo la petición no es instructor ni es administrador, no puede eliminar el cupon
+
+        if (user.role.type != "instructor" && user.role.type != "administrador") {
+
+            return ctx.unauthorized(`No tienes permisos para eliminar este cupon`);
+
+        }
+
+        //verifico que el cupon que se quiere eliminar pertenezca al usuario que lo quiere eliminar
+
+        const cupon = await strapi.db
+
+            .query("api::cupon.cupon")
+
+            .findOne({ where: { id }, populate: true });
+
+            console.log("Este es el cupon", cupon);
+
+        //si el usuario que hace la peticion no es el mismo que creo el cupon y no es administrador, envio un error
+
+        if (user.id != cupon.user.id && user.role.type != "administrador") {
+
+            return ctx.badRequest("Error", 
+
+                { messages: "No tienes permisos para eliminar el cupon" },
+
+            );
+
+        }
+
+        // si el usuario que está haciendo la petición es instructor o es administrador, puede eliminar el cupon
+
+        return await super.delete(ctx);
     }
 })); 
--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,64 +1,130 @@
-const { sanitize } = require('@strapi/utils');
+const { sanitize } = require("@strapi/utils");
 
 module.exports = (plugin) => {
+  plugin.controllers.user.getBySlug = async (ctx) => {
+    const { slug } = ctx.params;
 
-	plugin.controllers.user.getBySlug = async (ctx) => {
+    const entity = await strapi.db
+      .query("plugin::users-permissions.user")
+      .findOne({
+        where: { slug: slug },
+        // populo todos los	campos de la tabla
+        populate: true,
+      });
 
-		const { slug } = ctx.params;
+    // si no hay usuario retorno un error 404
 
+    if (!entity) {
+      return ctx.notFound("No se encontró el usuario");
+    }
 
+    // si hay usuario busco la meta_data del usuario
 
+    const meta = await strapi.db
+      .query("api::meta-usuario.meta-usuario")
+      .findOne({
+        where: { usuario: entity.id },
+        // populo todos los	campos de la tabla
+      });
 
-		const entity = await strapi.db.query('plugin::users-permissions.user').findOne({
-			where: { slug: slug },
-			// populo todos los	campos de la tabla
-			populate: true
-	})
+    //añado la meta_data al usuario
 
-		// si no hay usuario retorno un error 404 
+    entity.metaData = meta;
 
-		if (!entity) {
-			return ctx.notFound('No se encontró el usuario');
-		}
+    // elimino el password de la respuesta , updateBy y createBy
 
-		// si hay usuario busco la meta_data del usuario
+    let dataDelete = [
+      "password",
+      "updatedBy",
+      "createdBy",
+      "resetPasswordToken",
+      "confirmationToken",
+      "provider",
+    ];
 
-		const meta = await strapi.db.query('api::meta-usuario.meta-usuario').findOne({
-			where: { usuario: entity.id },
-			// populo todos los	campos de la tabla
-		})
+    // elimino los campos que no quiero que se muestren en la respuesta
 
-		//añado la meta_data al usuario 
+    dataDelete.forEach((item) => {
+      delete entity[item];
+    });
 
-		entity.metaData = meta;
+    return entity;
+  };
 
+  //controlador para cambiar el rol de un usuario de estudiante a instructor
+  plugin.controllers.user.roleInstructor = async (ctx) => {
+    console.log("entro al controlador");
+    //obtengo el id del usuario del token
+    const id = ctx.state.user.id;
 
+    console.log(id);
 
-	// elimino el password de la respuesta , updateBy y createBy
+    //busco el usuario
+    const entity = await strapi.db
+      .query("plugin::users-permissions.user")
+      .findOne({
+        where: { id: id },
+        // populo todos los	campos de la tabla
+        populate: true,
+      });
 
-	let dataDelete = ['password', 'updatedBy', 'createdBy', 'resetPasswordToken', 'confirmationToken', 'provider'];
+    let = message = [];
 
-	// elimino los campos que no quiero que se muestren en la respuesta
+    // si no hay usuario retorno un error 404
 
-	dataDelete.forEach((item) => {
-		delete entity[item];
-	});
-	
+    if (!entity) {
+      return ctx.notFound("No se encontró el usuario");
+      message.push("No se encontró el usuario");
+    }
+    //obtengo el rol del usuario
+    const role = entity.role.id;
 
+    //si el usuario ya es instructor retorno un error 400
 
+    if (entity.role.id == 3) {
+      return ctx.badRequest("El usuario ya es instructor");
+      message.push("El usuario ya es instructor");
+    }
 
-		return entity;
-	}
+    //si el usuario no es estudiante retorno un error 400
 
-	plugin.routes['content-api'].routes.push({
-		"method": "GET",
-		"path": "/users/slug/:slug",
-		"handler": "user.getBySlug",
-		"config": {
-			prefix: ''
-		}
+    if (entity.role.id != 1) {
+      return ctx.badRequest("El usuario no es estudiante");
+      message.push("El usuario no es estudiante");
+    }
 
-	})
+    //actualizo el rol del usuario y le asigno el rol de instructor
 
-	return plugin
-}
+    const entityActualizada = await strapi.db
+      .query("plugin::users-permissions.user")
+      .update({
+        where: { id: id },
+        data: {
+          role: 3,
+        },
+      });
+
+    console.log("Update", entityActualizada);
+    //retorno el usuario actualizado
+
+    return entityActualizada;
+  };
+
+  plugin.routes["content-api"].routes.push(
+    {
+      method: "GET",
+      path: "/users/slug/:slug",
+      handler: "user.getBySlug",
+      config: {
+        prefix: "",
+      },
+    },
+    {
+      method: "GET",
+      path: "/users/roleInstructor/",
+      handler: "user.roleInstructor",
+    }
+  );
+
+  return plugin;
+};
