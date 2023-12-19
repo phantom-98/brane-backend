536762bf5db144f822522e45b1f25913077592ab
Daniel <daniel@adaki.net>
2023-12-19 14:42:17 -0400

    add sentry

--- config/plugins.js
+++ config/plugins.js
@@ -62,4 +62,11 @@ module.exports = ({ env }) => ({
   "strapi-google-auth": {
     enabled: true,
   },
+  sentry: {
+    enabled: true,
+    config: {
+      dsn: env('SENTRY_DSN'),
+      sendMetadata: true,
+    },
+  },
 });
